3f88229119e12ed88d11685bd51e4638b289ec4e
Snekkem <snek9903@gmail.com>
2019-10-08 12:03:18 +0300

    about

--- Parser/Parser/spiders/parse_spider.py
+++ Parser/Parser/spiders/parse_spider.py
@@ -68,25 +68,31 @@ class ParseSpiderSpider(scrapy.Spider):
     def get_phonenumber(self, response):
         # A scraper designed to operate on one of the profile pages
         item = response.meta['item']  # Get the item we passed from scrape()
-        # item['rest_title'] = response.css('.heading--inline__373c0__1F-Z6').css('::text').extract_first()
-        # item['rest_phone'] = response.css('.text--offscreen__373c0__1SeFX+ .text-align--left__373c0__2pnx_').css(
-        #    '::text').extract_first()
-        # item['rest_reviews'] = response.css('.text-color--mid__373c0__3G312.text-size--large__373c0__1568g').css(
-        #     '::text').extract_first()
-        # item['rest_address'] = response.css('.island-section__373c0__3vKXy .text-align--left__373c0__2pnx_ '
-        #                                    '.lemon--span__373c0__3997G').css('::text').extract()
-        #  item['rest_timetable'] = response.css('.table-row__373c0__3wipe :nth-child(1) .text-align--left__373c0__2pnx_').css(
-        #      '::text').extract()
 
-        # item['rest_link'] = response.css('.text--offscreen__373c0__1SeFX+ .link-size--default__373c0__1skgq').css(
-        #     '::text').extract_first()
+        item['rest_title'] = response.css('.heading--inline__373c0__1F-Z6').css('::text').extract_first()
+        item['rest_phone'] = response.css('.text--offscreen__373c0__1SeFX+ .text-align--left__373c0__2pnx_').css(
+            '::text').extract_first()
+        item['rest_reviews'] = response.css('.text-color--mid__373c0__3G312.text-size--large__373c0__1568g').css(
+             '::text').extract_first()
+        item['rest_address'] = response.css('.island-section__373c0__3vKXy .text-align--left__373c0__2pnx_ '
+                                            '.lemon--span__373c0__3997G').css('::text').extract()
+        item['rest_timetable'] = response.css('.table-row__373c0__3wipe :nth-child(1) .text-align--left__373c0__2pnx_').css(
+              '::text').extract()
+
+        item['rest_link'] = response.css('.text--offscreen__373c0__1SeFX+ .link-size--default__373c0__1skgq').css(
+             '::text').extract_first()
 
         about = response.xpath(
             '//p[@class="lemon--p__373c0__3Qnnj text__373c0__2pB8f text-color--normal__373c0__K_MKN text-align--left__373c0__2pnx_"]')
-        about = about.xpath('//span[@width="0"]/text()').extract()
+        about = about.xpath('//span[@width="0"]').xpath("/span").extract_first()
         item['rest_about'] = about
 
         item['gym_title'] = response.css('.biz-page-title').css('::text').extract_first()
+
+        rev = response.css('.biz-rating-very-large .rating-qualifier').css('::text').extract_first()
+        rev = re.sub(r'(\\\\r|\\\\n|\\\\r\\\\n)*\s\s+', "", str(rev)).strip()
+        item['gym_reviews'] = rev
+
 
         some = response.xpath('//*[@id="wrap"]/div[2]/div/div[1]/div/div[4]/div[1]/div/div[2]/ul/li[1]/div/strong/address/text()').extract()
         some = re.sub(r'(\\\\r|\\\\n|\\\\r\\\\n|\\n|\\r)?|\s\s+', "", str(some)).strip()
@@ -97,6 +103,7 @@ class ParseSpiderSpider(scrapy.Spider):
         item['gym_phone'] = phone
         item['gym_link'] = response.css('.js-add-url-tagging a').css('::text').extract_first()
         table = response.xpath('//table[@class="table table-simple hours-table"]')
+
         times = table.xpath('//span[@class="nowrap"]/text()').extract()
         days = table.xpath('//th[@scope="row"]/text()').getall()
 
@@ -104,17 +111,21 @@ class ParseSpiderSpider(scrapy.Spider):
         temp = []
         result = []
 
-        for el in days:
-            listData.append(el)
+        try:
+            for el in range(5, 12):
+                listData.append(days[el])
 
-        for el in times:
-            temp.append(el)
+            for el in range(2, 16, 2):
+                temp.append(times[el] + ' ' + times[el + 1])
 
-        for el in range(7):
-            result.append(listData.index([el]) + temp.index([el]))
+            for el in range(7):
+                result.append(listData[el] + ' ' + temp[el])
+        except IndexError:
+            print('s')
 
         item['gym_timetable'] = result
 
         item['gym_link_img'] = response.css('.photo-2 .photo-box-img').css('::attr(src)').extract_first()
 
         yield item  # Return the new phonenumber'd item back to scrape
+    # scrapy crawl parse_spider
